c8efeaa356568771887f65b3a0d5e41cac749df2
Tom Donohoe <tom.donohoe@finder.com>
2021-12-26 17:12:07 +1100

    feat(): add connected players style (#23)
    adding styles for connected players component
    
    Related to CR-20

--- client/src/helpers/game.helpers.ts
+++ client/src/helpers/game.helpers.ts
@@ -32,3 +32,14 @@ export const handleCopyLinkClick = (event: any) => {
 
   navigator.clipboard.writeText(link);
 };
+
+export const generateRandomHexColor = (size: number): string => {
+  let result = [];
+  const hexRef = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
+
+  for (let n = 0; n < size; n++) {
+    result.push(hexRef[Math.floor(Math.random() * 16)]);
+  }
+
+  return result.join('');
+};
